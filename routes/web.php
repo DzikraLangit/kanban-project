178342271cf29d8532ea11608649902ad3940942
DzikraLangit <dzikralangit-student2021@smktelkom-lpg.sch.id>
2023-11-14 14:13:52 +0700

    :Apply common layout

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+use App\Http\Controllers\TaskController; // Ditambahkan
+
 Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/', function () {
 
-    return view('home'); // Diperbarui
+    return view('home');
 
-});
+})->name('home'); // name ditambahkan
 
+
+Route::get('/tasks/', [TaskController::class, 'index'])->name('tasks.index'); // name ditambahkan
